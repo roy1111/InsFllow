a2feac54de25e6d91b688ed511ff217702ae4131
roy1111 <30957505+roy1111@users.noreply.github.com>
2017-09-17 21:24:29 +0300

    Update InsFllow.py

--- InsFllow.py
+++ InsFllow.py
@@ -337,8 +337,8 @@ def handleExceptionAfterClick(Unfollowed):
         return Unfollowed
 
 
-username = 'puberty_goals.09'
-password = '158123RA'
+username = 'alpha__millionaire'
+password = '158158123'
 celebrityAccountURL = 'https://www.instagram.com/9gag/'
 
 GOOGLE_CHROME_BIN = r"/app/.apt/usr/bin/google-chrome"
